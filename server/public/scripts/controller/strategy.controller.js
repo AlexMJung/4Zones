8da54561c508da25a78cdd6c10044dd3cf3a9037
Alex Jung <alexandermjung89@gmail.com>
2017-09-17 21:11:22 -0500

    auto login working

--- server/public/scripts/controller/strategy.controller.js
+++ server/public/scripts/controller/strategy.controller.js
@@ -1,13 +1,11 @@
-app.controller('strategyController', ['loginService', "$location", function (loginService, $location) {
+app.controller('strategyController', ['loginService', "$location", "$mdDialog", function (loginService, $location, $mdDialog) {
     console.log('strategy.controller loaded');
     var self = this;
-    
+
     self.User = loginService.User
     console.log(self.User)
 
-    self.out = function(){
-        loginService.out();
-    }
+
 
 
     self.checkLoggedIn = function () {
@@ -15,6 +13,25 @@ app.controller('strategyController', ['loginService', "$location", function (log
             $location.path("/home")
         }
     }//end of checkLoggedIn
+
+    // self.showAlert = function (ev) {
+    //     $mdDialog.show(
+    //         $mdDialog.alert()
+    //             .parent(angular.element(document.querySelector('#background')))
+    //             .clickOutsideToClose(true)
+    //             .title('Great Job!')
+    //             //.textContent('You can specify some description text in here.')
+    //             .ariaLabel('Nice Work')
+    //             .ok('Thanks!')
+    //             .targetEvent(ev)
+    //     );
+
+
+    self.out = function () {
+        loginService.out();
+    }
+
+
 
     self.checkLoggedIn();
 }])//end of app.controller
