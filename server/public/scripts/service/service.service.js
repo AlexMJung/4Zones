390b156fcb3dc185692be51ddda09a22ca6e10c0
Alex Jung <alexandermjung89@gmail.com>
2017-09-19 09:56:34 -0500

    unsure of what is happening saving now to reset

---
+++ server/public/scripts/service/service.service.js
@@ -0,0 +1,15 @@
+app.service('serviceService', ['$http', '$location', function ($http, $location) {
+    var self = this;
+    console.log('login.service loaded');
+
+    self.Users = {list:[]};
+
+    self.getUsers = function (){
+        console.log("self.getUsers hit in service");
+        $http.get('/getUsers').then(function (response) {
+            self.Users.list= response.data
+
+        })}
+
+
+}]);
